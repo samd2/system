c306d479e8326a68d07ee7f058a415fe8b67429b
Peter Dimov <pdimov@pdimov.com>
2017-06-24 03:45:04 +0300

    Update Jamfile to generate dependencies

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,5 +1,8 @@
+# Generated file. Do not edit.
+
 boost_declare_dependency(boost_assert PUBLIC boost::assert)
 boost_declare_dependency(boost_config PUBLIC boost::config)
 boost_declare_dependency(boost_core PUBLIC boost::core)
 boost_declare_dependency(boost_predef PUBLIC boost::predef)
 boost_declare_dependency(boost_winapi PUBLIC boost::winapi)
+
